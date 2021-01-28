2656fd6b5e365c6753fcb1f6395662e344cc776f
luciano.ramello <luciano.ramello@vass.es>
2021-01-28 23:45:16 +0100

    change config

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ device_id = 'BCM2835-00000000ec2a41ff-9000c1'
 # MQ135 : Air quality sensor based on SnO2 conductivity measurement
 # TODO : include in the documentation the unit of measure for each indicator
 dht11 = 1
-bmp180 = 0
+bmp180 = 1
 mq135 = 0
 
 # TODO: next sensors to integrate
